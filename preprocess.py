fd09e98802ac4990a0dfce4b37403c4d7c6561d9
Matthias Lee <matthias.a.lee@gmail.com>
2012-04-15 17:29:02 -0400

    basic text processing working: ie read in and strip out certain characters

--- preprocess.py
+++ preprocess.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python
 import glob
+import re
+
+def cleanText(inText):
+	# strip out whatever we need here 
+	p = re.compile('[,]')
+	outText = p.sub('',inText.lower())
+	return outText
+
+def processFile(fname):
+	print "processing:",fname
+	f = open(fname, "r");
+	lines=""
+	for line in f.readlines():
+		lines+=line.strip()+" "
+	lines = cleanText(lines)
+	# TODO: split by sentence?
+	return lines
 
 def main(dataDir, langDirs):
 	for l in langDirs:
+		cnt=0
 		for f in glob.glob(dataDir + "/" + l + "/*"):
 			# chop out non input files.. aka readme
 			if f.find(".txt") > 0:
-				print f
+				cnt+=1
+				print processFile(f)
+				exit()
+
+		print l, cnt, "input files"
 
 if __name__ == "__main__":
 	langs=["en","es"]
