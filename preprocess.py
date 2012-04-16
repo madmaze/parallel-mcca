f074da0dbd01f5b4aee238db51e19e4dbbadabc0
Matthias Lee <matthias.a.lee@gmail.com>
2012-04-15 22:24:00 -0400

    added gitignore and saveProcessed()

--- preprocess.py
+++ preprocess.py
@@ -8,6 +8,13 @@ def cleanText(inText):
 	outText = p.sub('',inText)
 	return outText
 
+def saveProcessed(data,fname):
+	fout = open(fname+".processed", "w")
+	for line in data:
+		print line
+		fout.write(line+"\n")
+		
+
 def processFile(fname):
 	print "processing:",fname
 	f = open(fname, "r");
@@ -61,7 +68,8 @@ def main(dataDir, langDirs):
 			# chop out non input files.. aka readme
 			if f.find(".txt") > 0:
 				cnt+=1
-				print processFile(f)
+				saveProcessed(processFile(f),"PROCESSED/"+l+"/"+str(cnt))
+				
 				exit()
 
 		print l, cnt, "input files"
