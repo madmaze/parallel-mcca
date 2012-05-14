bdded029f9eb68d5ccdf4eba5853fdbe52060e23
Matthias Lee <matthias.a.lee@gmail.com>
2012-05-14 00:07:23 -0400

    fixed encoding things

--- data.py
+++ data.py
@@ -115,7 +115,7 @@ class data:
 		self.esVecs.cleanupVector()
 		self.enVecs.transfromVector()
 		self.esVecs.transfromVector()
-		self.esVecs.getTestVectors("german.2.part","DICT/german.1.part")
+		#self.esVecs.getTestVectors("german.2.part","DICT/german.1.part")
 
 	def preprocess(self):
 		print "in preprocessing().."
