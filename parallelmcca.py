8096362c397100b45db27f6abaa11de0cdd2b84a
Matthias Lee <matthias.a.lee@gmail.com>
2012-05-15 04:56:05 -0700

    association vecotr calculations working, next make run for all

--- parallelmcca.py
+++ parallelmcca.py
@@ -11,37 +11,41 @@ class gpuProcessor:
 				[10.0,15.0,6.0],
 				[10.0,15.0,7.0],
 				[10.0,15.0,5.0]],numpy.float32)
+		itemCnt=a.shape[0]
 		
+		# Allocate input array
 		a_gpu = cuda.mem_alloc(a.size * a.dtype.itemsize)
-		dest = numpy.zeros_like(a)
-		dest_gpu = cuda.mem_alloc(a.size * a.dtype.itemsize)
+		# Allocate results array Host & Device
+		dest = numpy.zeros(shape=(itemCnt,1),dtype=numpy.float32)
+		dest_gpu = cuda.mem_alloc(itemCnt * a.dtype.itemsize)
+		
 		
 		cuda.memcpy_htod(a_gpu, a)
 		
 		mod = SourceModule("""
 		    __global__ void doublify(float *dest, float *a, float cSize)
 		    {
-		      int tid = threadIdx.x * 3;
+		      int tid = threadIdx.x*3;
 
-		      float k11 = a[tid+3];
-		      float k12 = a[tid+2] - k11;
-		      float k21 = a[tid+1] - k11;
-		      float k22 = cSize - a[tid+2] - a[tid+1];
+		      float k11 = a[tid+2];
+		      float k12 = a[tid+1] - k11;
+		      float k21 = a[tid] - k11;
+		      float k22 = cSize - a[tid+1] - a[tid];
 		      float C1 = k11 + k12;
 		      float C2 = k21 + k22;
 		      float R1 = k11 + k21;
 		      float R2 = k12 + k22;
 		      float N = k11 + k12 + k21 + k22;
 		      
-		      dest[tid] = k11*log((k11*N)/(C1*R1));// + k12*log((k12*N)/(C1*R2)) + k21*log((k21*N)/(C2*R1)) + k22*log((k22*N)/(C2*R2));
+		      dest[threadIdx.x] = k11*log((k11*N)/(C1*R1)) + k12*log((k12*N)/(C1*R2)) + k21*log((k21*N)/(C2*R1)) + k22*log((k22*N)/(C2*R2));
 		    }
 		    """)
 		
 		func = mod.get_function("doublify")
-		cSize=1450
+		cSize=numpy.float32(1450)
 		func(dest_gpu,a_gpu,cSize, block=(4,1,1))
 		
-		a_doubled = numpy.empty_like(a)
+		#a_doubled = numpy.empty_like(a)
 		cuda.memcpy_dtoh(dest, dest_gpu)
 		
 		print "original array:"
